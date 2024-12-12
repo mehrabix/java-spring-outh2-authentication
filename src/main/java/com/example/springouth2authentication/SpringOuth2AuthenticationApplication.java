9d441e6a9cab172fbacb2cf054431fe83d8de03e
aenima <aenima@aenimas-MacBook-Pro.local>
2024-12-12 21:08:47 +0330

    init spring project

---
+++ src/main/java/com/example/springouth2authentication/SpringOuth2AuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springouth2authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOuth2AuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOuth2AuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springouth2authentication/SpringOuth2AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springouth2authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOuth2AuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
